6542904a7fc201517e0e3603d533a098b0f38219
Wahib Kerkeni <mr.wahib@gmail.com>
2022-08-27 13:03:34 +0100

    Added Custom Header Authentication Filter

--- src/test/java/guru/sfg/brewery/web/controllers/BeerRestControllerIT.java
+++ src/test/java/guru/sfg/brewery/web/controllers/BeerRestControllerIT.java
@@ -3,6 +3,7 @@ package guru.sfg.brewery.web.controllers;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -24,6 +25,14 @@ public class BeerRestControllerIT extends BaseIT {
     @Test
     void findBeerByUpc() throws Exception {
         mockMvc.perform(get("/api/v1/beerUpc/0631234200036"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void deleteBeer() throws Exception {
+        mockMvc.perform(delete("/api/v1/beer/79b9adb9-d8c9-4802-bcfb-9041934fcd86")
+                        .header("Api-Key", "spring")
+                        .header("Api-Secret", "spring"))
                 .andExpect(status().isOk());
     }
 }
