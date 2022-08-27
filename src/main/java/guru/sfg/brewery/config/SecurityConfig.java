19a332a39fa2320601e5b25ea8e0312f4649bc44
Wahib Kerkeni <mr.wahib@gmail.com>
2022-08-27 19:57:36 +0100

    Added Allowing multiple roles

--- src/main/java/guru/sfg/brewery/config/SecurityConfig.java
+++ src/main/java/guru/sfg/brewery/config/SecurityConfig.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -47,7 +46,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                             .antMatchers("/h2-console/**").permitAll() // do not use in production
                             .antMatchers(HttpMethod.GET, "/api/v1/beer/**").permitAll()
                             .mvcMatchers(HttpMethod.GET, "/api/v1/beerUpc/{upc}").permitAll()
-                            .mvcMatchers(HttpMethod.DELETE,"/api/v1/beer/**").hasRole("ADMIN");
+                            .mvcMatchers("/brewery/breweries")
+                            .hasAnyRole("ADMIN", "CUSTOMER")
+                            .mvcMatchers(HttpMethod.GET, "/brewery/api/v1/breweries")
+                            .hasAnyRole("ADMIN", "CUSTOMER")
+                            .mvcMatchers(HttpMethod.DELETE, "/api/v1/beer/**")
+                            .hasRole("ADMIN");
                 })
                 .authorizeRequests()
                 .anyRequest().authenticated()
