2f555bb7f898b1ca04b9c0bdacc9b83665f2920c
Wahib Kerkeni <mr.wahib@gmail.com>
2022-08-28 14:24:05 +0100

    Added custom permissions Annotations

---
+++ src/main/java/guru/sfg/brewery/security/perms/customers/CustomerCreatePermission.java
@@ -0,0 +1,12 @@
+package guru.sfg.brewery.security.perms.customers;
+
+import guru.sfg.brewery.constants.Permissions;
+import org.springframework.security.access.prepost.PreAuthorize;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+@PreAuthorize("hasAuthority('" + Permissions.CUSTOMER_CREATE + "')")
+public @interface CustomerCreatePermission {
+}
